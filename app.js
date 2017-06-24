b0fb1e0dbae383898403605a242d54ce3d04a46b
Tyler Reitz <tdreitz@gmail.com>
2017-06-23 23:23:15 -0700

    updated PORT & added package.json

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const path = require('path')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
